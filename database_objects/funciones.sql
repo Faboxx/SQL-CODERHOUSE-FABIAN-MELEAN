78067ddb05f1aec89670449c976e1ee6de22508e
Faboxx <meleanfabian@gmail.com>
2024-08-04 13:48:42 -0300

    funciones.sql

--- database_objects/funciones.sql
+++ database_objects/funciones.sql
@@ -1,1 +1,42 @@
 
+-- Función para chequear todos los productos vendidos.
+
+DROP FUNCTION IF EXISTS Ecommerce.cantidad_vendida_por_producto;
+
+DELIMITER //
+
+CREATE FUNCTION cantidad_vendida_por_equipo(idproducto INT)
+RETURNS INT
+DETERMINISTIC
+BEGIN
+    DECLARE cantidad_vendida INT;
+    
+    SELECT SUM(cantidad) INTO cantidad_vendida
+    FROM VENTAS
+    WHERE idproducto = idproducto;
+    
+    RETURN cantidad_vendida;
+END //
+
+DELIMITER ;
+
+-- Funcion para contar las compras realizadas por un cliente.
+
+DELIMITER //
+
+CREATE FUNCTION compras_cliente_total (idcliente INT, fecha_inicio DATETIME, fecha_fin DATETIME) RETURNS INT
+DETERMINISTIC
+READS SQL DATA
+BEGIN
+    DECLARE compras_cliente_count INT;
+    
+    SELECT COUNT(*) INTO compras_cliente_count
+    FROM PEDIDOS
+    WHERE IDCLIENTE = cliente_id
+    AND FECHA >= fecha_inicio
+    AND FECHA <= fecha_fin;
+    
+    RETURN reservas_count;
+END //
+
+DELIMITER ;

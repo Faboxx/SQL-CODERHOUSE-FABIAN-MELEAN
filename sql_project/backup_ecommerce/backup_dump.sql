50b6b29608b1896774316be4f0d2127b55ff9ab2
Faboxx <meleanfabian@gmail.com>
2024-08-30 19:58:12 -0300

    Update backup_dump.sql

--- sql_project/backup_ecommerce/backup_dump.sql
+++ sql_project/backup_ecommerce/backup_dump.sql
@@ -4,7 +4,7 @@ USE `ecommerce`;
 --
 -- Host: localhost    Database: ecommerce
 -- ------------------------------------------------------
--- Server version	8.0.37
+-- Server version	8.4.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -541,4 +541,4 @@ SET character_set_client = @saved_cs_client;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2024-08-30 13:13:52
+-- Dump completed on 2024-08-30 19:57:08
